ee3880e59b79dc6bb1894a9c4905d830f5e43dbb
Leksa <surnacheva@ithub.ru>
2021-05-04 16:59:13 +0300

    update lightbox

--- 2021.05.04/script.js
+++ 2021.05.04/script.js
@@ -10,4 +10,33 @@ window.addEventListener("load", e => {
         el.style.backgroundImage = `url(${img})`;
         container.appendChild(el);
     });
+
+    //////////////////////////////////////////////////
+
+    const lbx = document.querySelector(".lightbox");
+    const cross = lbx.querySelector(".cross");
+    const next = lbx.querySelector(".arrow.right");
+    const prev = lbx.querySelector(".arrow.left");
+    const cards = document.querySelectorAll(".card");
+
+    const showLightbox = (e) => {
+        document.body.classList.add("light");
+        lbx.firstElementChild.style.backgroundImage = `url(${e.target.getAttribute("data-src")})`;
+    }
+    const hideLightbox = () => {
+        document.body.classList.remove("light");
+    }
+
+
+    cards.forEach(card => {
+        card.addEventListener("click", showLightbox);
+    });
+    cross.addEventListener("click", hideLightbox);
+    document.body.addEventListener("keydown", (e) => {
+        // console.log(e.key);
+        if (e.key === "Escape") {
+            hideLightbox();
+        }
+    })
+
 });
